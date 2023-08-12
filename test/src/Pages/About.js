05b4b62e1880b8352611d41fa92033cef2bc7e65
Jeff Rosanes <rosjeffe@gmail.com>
2023-08-12 17:18:00 +0800

    test

---
+++ test/src/Pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../Layout/header";
+// import Content from "../layout/content";
+import Footer from "../Layout/footer"
+
+const About = () =>{
+    return(
+      <div>
+<Header></Header>
+<p>
+    This is my AboutUs us
+</p>
+<Footer />
+      </div>
+      
+
+       
+        
+    )
+};
+export default About;

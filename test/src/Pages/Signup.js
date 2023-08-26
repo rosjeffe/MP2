969fa7a2632a353842723bccf20a8996a2ee309b
jansmsardina <jsmsardina.cns@gmail.com>
2023-08-26 13:34:47 +0800

    sign up card

--- test/src/Pages/Signup.js
+++ test/src/Pages/Signup.js
@@ -1,4 +1,5 @@
 import Header from "../Layout/header";
+import Footer from "../Layout/footer";
 import React, { useState } from 'react';
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
@@ -57,56 +58,72 @@ function Signup() {
   return (
     <div>
       <Header />
+      <div id="regForm">
       <Container>
-        <h2>Store Registration</h2>
+        <h2 id="regHead">Store Registration</h2>
         <Form className="regForm" onSubmit={handleSubmit}>
           <Form.Group>
+            <div className="formLabel">
             <Form.Label>Store Name</Form.Label>
+            </div>
             <Form.Control type="text" style={{width:'50%'}} value={name} onChange={handleNameChange} placeholder="Enter store name" />
           </Form.Group>
           <Form.Group>
+          <div className="formLabel">
             <Form.Label>Username</Form.Label>
+            </div>
             <Form.Control type="text"style={{width:'50%'}} value={user} onChange={handleUserChange} placeholder="Enter username" />
           </Form.Group>
           <Form.Group>
+          <div className="formLabel">
             <Form.Label>Password</Form.Label>
+            </div>
             <Form.Control type="password"style={{width:'50%'}} value={password} onChange={handlePasswordChange} placeholder="Enter password" />
           </Form.Group>
           <Form.Group>
+          <div className="formLabel">
             <Form.Label>Email</Form.Label>
+            </div>
             <Form.Control type="email" style={{width:'50%'}} value={email} onChange={handleEmailChange} placeholder="Enter email" />
           </Form.Group>
           <Form.Group>
+          <div className="formLabel">
             <Form.Label>Upload Menu (PDF)</Form.Label>
+            </div>
             <Form.Control type="file" style={{width:'50%'}}  value={menuFile} onChange={handleFileChange} />
           </Form.Group>
-          <Row>
-            <Col>
               <Form.Group>
+              <div className="formLabel">
+              </div>
                 <Form.Label>Opening Time</Form.Label>
-                <Form.Control type="time"   style={{ width: '25%' }} value={open} onChange={handleOpeningChange} />
+                <Form.Control type="time"  style={{width:'50%'}}  value={open} onChange={handleOpeningChange} />
               </Form.Group>
-            </Col>
-            <Col>
               <Form.Group>
+              <div className="formLabel">
+              </div>
                 <Form.Label>Closing Time</Form.Label>
-                <Form.Control type="time" style={{width:'25%', marginLeft: '-50%'}} value={close} onChange={handleClosingChange} />
+                <Form.Control type="time" style={{width:'50%'}} value={close} onChange={handleClosingChange} />
               </Form.Group>
-            </Col>
-          </Row>
+          
           <Form.Group>
+          <div className="formLabel">
             <Form.Label>Address</Form.Label>
-            <Form.Control type="text"style={{width:'50%'}} value={address} onChange={handleAddressChange} placeholder="Enter Complete Address" />
+            </div>
+            <Form.Control type="text" style={{width:'50%'}} value={address} onChange={handleAddressChange} placeholder="Enter Complete Address" />
           </Form.Group>
           <Form.Group>
+          <div className="formLabel">
             <Form.Label>Contact Number</Form.Label>
-            <Form.Control type="text"style={{width:'50%'}} value={contact} onChange={handleUserChange} placeholder="Enter Contact Number" />
+            </div>
+            <Form.Control type="text" style={{width:'50%'}} value={contact} onChange={handleUserChange} placeholder="Enter Contact Number" />
           </Form.Group>
           <br/>
           
           <Button type="submit" >Register</Button>
         </Form>
       </Container>
+      </div>
+      <Footer />
     </div>
   );
 }
